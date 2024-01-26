067f754de92ede5009767d5833a02796c2bc57c4
Logandevries <129877889+Logandevries@users.noreply.github.com>
2024-01-26 02:27:45 -0800

    Update journal1-5.py

--- journal1-5.py
+++ journal1-5.py
@@ -3,6 +3,6 @@ import numpy as np
 x_values = np.linspace(0, 2, 1000)
 sine_values = np.sin(x_values)
 
-combined_values = np.column_stack((x_values, sine_values))
+both_values = np.column_stack((x_values, sine_values))
 
-print(combined_values)
+print(both_values)
